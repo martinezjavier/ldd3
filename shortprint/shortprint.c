21e7e3cca0c5a1ef7b3e5a9c85df05c92f23596f
Javier Martinez Canillas <martinez.javier@gmail.com>
2010-12-08 02:28:16 +0100

    shortprint: Remove third argument from macro DECLARE_WORK()

--- shortprint/shortprint.c
+++ shortprint/shortprint.c
@@ -108,7 +108,7 @@ static DECLARE_WAIT_QUEUE_HEAD(shortp_out_queue);
  * workqueue.
  */
 static void shortp_do_work(void *);
-static DECLARE_WORK(shortp_work, shortp_do_work, NULL);
+static DECLARE_WORK(shortp_work, shortp_do_work);
 static struct workqueue_struct *shortp_workqueue;
 
 /*
