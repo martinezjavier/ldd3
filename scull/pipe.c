a0556585eecb5601bb3ac467013e44fa077ce274
Javier Martinez Canillas <martinez.javier@gmail.com>
2011-01-03 14:05:47 +0100

    scull: pipe: Use sema_init() instead init_MUTEX()

--- scull/pipe.c
+++ scull/pipe.c
@@ -363,7 +363,7 @@ int scull_p_init(dev_t firstdev)
 	for (i = 0; i < scull_p_nr_devs; i++) {
 		init_waitqueue_head(&(scull_p_devices[i].inq));
 		init_waitqueue_head(&(scull_p_devices[i].outq));
-		init_MUTEX(&scull_p_devices[i].sem);
+		sema_init(&scull_p_devices[i].sem, 1);
 		scull_p_setup_cdev(scull_p_devices + i, i);
 	}
 #ifdef SCULL_DEBUG
