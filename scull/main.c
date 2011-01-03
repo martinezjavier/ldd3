de1bfbd379b4301762e06962b5566c2e7fe47f83
Javier Martinez Canillas <martinez.javier@gmail.com>
2011-01-03 14:02:56 +0100

    scull: BKL ioctl pushdown API change and use sema_init() instead init_MUTEX()

--- scull/main.c
+++ scull/main.c
@@ -389,8 +389,8 @@ ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
  * The ioctl() implementation
  */
 
-int scull_ioctl(struct inode *inode, struct file *filp,
-                 unsigned int cmd, unsigned long arg)
+int scull_ioctl(struct file *filp, unsigned int cmd,
+		unsigned long arg)
 {
 
 	int err = 0, tmp;
@@ -552,7 +552,7 @@ struct file_operations scull_fops = {
 	.llseek =   scull_llseek,
 	.read =     scull_read,
 	.write =    scull_write,
-	.ioctl =    scull_ioctl,
+	.unlocked_ioctl = scull_ioctl,
 	.open =     scull_open,
 	.release =  scull_release,
 };
@@ -648,7 +648,7 @@ int scull_init_module(void)
 	for (i = 0; i < scull_nr_devs; i++) {
 		scull_devices[i].quantum = scull_quantum;
 		scull_devices[i].qset = scull_qset;
-		init_MUTEX(&scull_devices[i].sem);
+		sema_init(&scull_devices[i].sem, 1);
 		scull_setup_cdev(&scull_devices[i], i);
 	}
 
