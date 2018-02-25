d7ec71f703e12cec7bd6b14a7b920a01d2c90886
Javier Martinez Canillas <javier@dowhile0.org>
2018-02-25 23:20:44 +0100

    scull: Include <linux/uaccess.h> header for copy_*_user()
    Signed-off-by: Javier Martinez Canillas <javier@dowhile0.org>

--- scull/main.c
+++ scull/main.c
@@ -28,7 +28,7 @@
 #include <linux/seq_file.h>
 #include <linux/cdev.h>
 
-#include <asm/uaccess.h>	/* copy_*_user */
+#include <linux/uaccess.h>	/* copy_*_user */
 
 #include "scull.h"		/* local definitions */
 
