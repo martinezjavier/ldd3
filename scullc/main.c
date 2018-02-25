b86423a5217ca616feb4b65a5bb02596f08d227d
Javier Martinez Canillas <javier@dowhile0.org>
2018-02-25 23:29:19 +0100

    scullc: Include <linux/uaccess.h> header for copy_*_user()
    Signed-off-by: Javier Martinez Canillas <javier@dowhile0.org>

--- scullc/main.c
+++ scullc/main.c
@@ -27,7 +27,7 @@
 #include <linux/seq_file.h>
 #include <linux/fcntl.h>	/* O_ACCMODE */
 #include <linux/aio.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include "scullc.h"		/* local definitions */
 
 
