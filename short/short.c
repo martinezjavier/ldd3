c600b086ac0a10c5ac0a64fb027e99f8f4da399c
Javier Martinez Canillas <javier@dowhile0.org>
2018-02-25 23:37:05 +0100

    short: Don't use deprecated IRQF_DISABLED flag when requesting a IRQ
    The IRQF_DISABLED flag has been removed from the Linux kernel, remove
    it from the driver so it compiles again.
    
    Signed-off-by: Javier Martinez Canillas <javier@dowhile0.org>

--- short/short.c
+++ short/short.c
@@ -511,7 +511,7 @@ void short_selfprobe(void)
       */
 	for (i = 0; trials[i]; i++)
 		tried[i] = request_irq(trials[i], short_probing,
-				IRQF_DISABLED, "short probe", NULL);
+				       0, "short probe", NULL);
 
 	do {
 		short_irq = 0; /* none got, yet */
@@ -621,7 +621,7 @@ int short_init(void)
 	 */
 	if (short_irq >= 0 && share > 0) {
 		result = request_irq(short_irq, short_sh_interrupt,
-				IRQF_SHARED | IRQF_DISABLED,"short",
+				     IRQF_SHARED,"short",
 				short_sh_interrupt);
 		if (result) {
 			printk(KERN_INFO "short: can't get assigned irq %i\n", short_irq);
@@ -635,7 +635,7 @@ int short_init(void)
 
 	if (short_irq >= 0) {
 		result = request_irq(short_irq, short_interrupt,
-				IRQF_DISABLED, "short", NULL);
+				     0, "short", NULL);
 		if (result) {
 			printk(KERN_INFO "short: can't get assigned irq %i\n",
 					short_irq);
@@ -655,7 +655,7 @@ int short_init(void)
 		result = request_irq(short_irq,
 				tasklet ? short_tl_interrupt :
 				short_wq_interrupt,
-				IRQF_DISABLED,"short-bh", NULL);
+				0, "short-bh", NULL);
 		if (result) {
 			printk(KERN_INFO "short-bh: can't get assigned irq %i\n",
 					short_irq);
