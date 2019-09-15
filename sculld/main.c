9bef10133543ee0656ffb88816df2501b99ab95e
Dan Walkes <danwalkes@trellis-logic.com>
2019-09-14 18:00:53 -0600

    Use kernel 5.0 version of access_ok
    See [this update](https://lkml.org/lkml/2019/1/4/418) which removed the
    type argument.

--- sculld/main.c
+++ sculld/main.c
@@ -289,9 +289,9 @@ long sculld_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 	 * "write" is reversed
 	 */
 	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		err =  !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
+		err =  !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	if (err)
 		return -EFAULT;
 
