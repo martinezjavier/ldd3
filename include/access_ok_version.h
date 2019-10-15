331c08c802361d9d9e1aebc9f5f8dbf85d475051
Dan Walkes <danwalkes@trellis-logic.com>
2019-10-15 08:30:34 -0600

    Fix wrappers for access_ok to work on 5.0 kernels

--- include/access_ok_version.h
+++ include/access_ok_version.h
@@ -1,31 +1,15 @@
 /*
  * @file access_ok_version.h
  * @date 10/13/2019
- * 
+ *
  */
 
-
-#include <linux/uaccess.h>
 #include <linux/version.h>
 
-
-/*
- * Wrapper function which drops the third argument on kernel 5.0 or later
- */
-
-inline int access_ok_wrapper(int type, void __user * arg, unsigned int cmd)
-{
-	int err =0;
-
-	#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
-
-	err = access_ok(type, (void __user *)arg, cmd);
-
-	#else
-
-	err = access_ok((void __user *)arg, cmd);
-
-	#endif
-
-	return err;
-}
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
+#define access_ok_wrapper(type,arg,cmd) \
+	access_ok(type, arg, cmd)
+#else
+#define access_ok_wrapper(type,arg,cmd) \
+	access_ok(arg, cmd)
+#endif
